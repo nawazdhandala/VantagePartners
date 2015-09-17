bc78bf3f5cdfb815f587e44a82ca36e52dcd8724
Ranjeet <ranjeetkumar692@gmail.com>
2015-09-17 12:03:31 +0530

    broadcast fixed

--- worker.js
+++ worker.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var rpc = require('avs-rpc');
 var sockets = [];
+var senderId = null;
 var dataStore = [{"PersonID":"abcd123", "FirstName": "Sample1","LastName": "Cliente 1","Country": "1 - Cliente 1"},{"PersonID":"abcd124", "FirstName": "Sample2","LastName": "Cliente 2","Country": "1 - Cliente 1"}];
 
 
@@ -29,11 +30,14 @@ module.exports.run = function (worker) {
   scServer.on('connection', function (socket) {
     sockets.push(socket);
 
+
+
     //implement RPC
     var serverRPC = new rpc.scRpc(socket);
     serverRPC.implementAsync(local);
 
     socket.on('test', function (data, res){
+      senderId = socket.id;
       if(data.type === "read"){
           data.data = dataStore;
           console.log("read");
@@ -44,7 +48,7 @@ module.exports.run = function (worker) {
           console.log("update" + JSON.stringify(data));
           data.type = "push-update";
           //scServer.emit('test', data);
-          broadcast('test', data);
+          broadcast('test',senderId, data);
           res(null, data);
 
       }else if(data.type === "destroy"){
@@ -52,17 +56,17 @@ module.exports.run = function (worker) {
         console.log("destroy" + JSON.stringify(data));
         data.type = "push-destroy";
         //scServer.emit('test', data);
-        broadcast('test', data);
+        broadcast('test', senderId, data);
         res(null, data);
 
       }else if(data.type === "create"){
+
         data.data[0].PersonID = data.id;
-        data.data[0].FirstName = "ABc";
         console.log("create " + JSON.stringify(data));
         data.type = "push-create";
         //scServer.emit('test', data);
 
-        broadcast('test', data);
+        broadcast('test', senderId, data);
         res(null, data);
 
       }
@@ -70,8 +74,9 @@ module.exports.run = function (worker) {
   });
 };
 
-function broadcast(event, data) {
+function broadcast(event, senderId, data) {
   sockets.forEach(function (socket) {
-    socket.emit(event, data);
+    if(socket.id !== senderId)
+      socket.emit(event, data);
   });
 }
